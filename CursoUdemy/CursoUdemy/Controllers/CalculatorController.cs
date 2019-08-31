abe272b13253b60ec16a86eab383dd9fe26a5702
Rodrigo Flores de Aquino <rodrigoaquinogalo13@hotmail.com>
2019-08-31 15:59:17 -0300

    Implementação

---
+++ CursoUdemy/CursoUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{firtsNumber}/{secondNumber}")]
+        public IActionResult Sum(string firtsNumber, string secondNumber)
+        {
+            if (IsNumeric(firtsNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firtsNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
